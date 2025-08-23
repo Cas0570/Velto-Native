601efc995c32ca0a1659133ad3ff6cfd2162c4ef
Cas Doorn <casdoorn.system@gmail.com>
2025-08-23 23:47:03 +0200

    fix: fixed linting issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
   theme: {
     extend: {
       fontFamily: {
-        Jakarta: ["Jakarta", "sans-serif"],
-        JakartaBold: ["Jakarta-Bold", "sans-serif"],
-        JakartaExtraBold: ["Jakarta-ExtraBold", "sans-serif"],
-        JakartaExtraLight: ["Jakarta-ExtraLight", "sans-serif"],
-        JakartaLight: ["Jakarta-Light", "sans-serif"],
-        JakartaMedium: ["Jakarta-Medium", "sans-serif"],
-        JakartaSemiBold: ["Jakarta-SemiBold", "sans-serif"],
+        Jakarta: ['Jakarta', 'sans-serif'],
+        JakartaBold: ['Jakarta-Bold', 'sans-serif'],
+        JakartaExtraBold: ['Jakarta-ExtraBold', 'sans-serif'],
+        JakartaExtraLight: ['Jakarta-ExtraLight', 'sans-serif'],
+        JakartaLight: ['Jakarta-Light', 'sans-serif'],
+        JakartaMedium: ['Jakarta-Medium', 'sans-serif'],
+        JakartaSemiBold: ['Jakarta-SemiBold', 'sans-serif'],
       },
       colors: {
         primary: {
@@ -23,7 +23,7 @@ module.exports = {
           600: '#36aa60',
           700: '#287f48',
           800: '#1b5530',
-          900: '#0d2a18'
+          900: '#0d2a18',
         },
         secondary: {
           100: '#fefefe',
@@ -34,20 +34,20 @@ module.exports = {
           600: '#c4c4c4',
           700: '#939393',
           800: '#626262',
-          900: '#313131'
+          900: '#313131',
         },
         general: {
-          100: "#CED1DD",
-          200: "#858585",
-          300: "#EEEEEE",
-          400: "#0CC25F",
-          500: "#F6F8FA",
-          600: "#E6F3FF",
-          700: "#EBEBEB",
-          800: "#ADADAD",
+          100: '#CED1DD',
+          200: '#858585',
+          300: '#EEEEEE',
+          400: '#0CC25F',
+          500: '#F6F8FA',
+          600: '#E6F3FF',
+          700: '#EBEBEB',
+          800: '#ADADAD',
         },
-      }
+      },
     },
   },
   plugins: [],
-}
+};
